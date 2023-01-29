dedff25c17a74908e8109701e0335e4cc98f4eca
romanpast <pastroman98@gmail.com>
2023-01-29 22:01:51 +0900

    initial

---
+++ module9-task1/main.js
@@ -0,0 +1,14 @@
+const iconPath = document.getElementById("iconPath");
+const btn = document.querySelector(".btn");
+
+
+const changeIcon = () => {
+    if (iconPath.getAttribute("d") == "M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-5.904-2.854a.5.5 0 1 1 .707.708L6.707 9.95h2.768a.5.5 0 1 1 0 1H5.5a.5.5 0 0 1-.5-.5V6.475a.5.5 0 1 1 1 0v2.768l4.096-4.097z") {
+        iconPath.setAttribute("d", "M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-5.904-2.803a.5.5 0 1 1 .707.707L6.707 10h2.768a.5.5 0 0 1 0 1H5.5a.5.5 0 0 1-.5-.5V6.525a.5.5 0 0 1 1 0v2.768l4.096-4.096z")
+    } else {
+        iconPath.setAttribute("d", "M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-5.904-2.854a.5.5 0 1 1 .707.708L6.707 9.95h2.768a.5.5 0 1 1 0 1H5.5a.5.5 0 0 1-.5-.5V6.475a.5.5 0 1 1 1 0v2.768l4.096-4.097z")
+    }
+}
+
+btn.addEventListener("click", changeIcon);
+
