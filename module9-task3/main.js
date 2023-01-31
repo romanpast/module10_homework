ce8be0fa2200b067c559fd8b938ffa58e0d7512c
romanpast <pastroman98@gmail.com>
2023-01-31 20:40:36 +0900

    3

---
+++ module9-task3/main.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+    alert("The screen width is " + window.screen.width + "px" + "\n" + "The screen height is " + window.screen.height + "px")
+})
