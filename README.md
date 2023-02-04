edbafcbfbd4d9a2c587fa166f9b2cd39078ed481
romanpast <pastroman98@gmail.com>
2023-02-04 21:17:28 +0900

    done

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 
 [task №1 GitHub page:](https://romanpast.github.io/module9_homework/module9-task1/index.html)
-https://romanpast.github.io/module8_homework/module8-task3/index.html
+https://romanpast.github.io/module9_homework/module9-task1/index.html
 
 [task №2 GitHub page:](https://romanpast.github.io/module9_homework/module9-task2/index.html)
-https://romanpast.github.io/module8_homework/module8-task4/index.html
+https://romanpast.github.io/module9_homework/module9-task2/index.html
 
 [task №3 GitHub page:](https://romanpast.github.io/module9_homework/module9-task3/index.html)
-https://romanpast.github.io/module8_homework/module8-task5/index.html
+https://romanpast.github.io/module9_homework/module9-task3/index.html
 
-test
--- module9-task3/main.js
+++ module9-task3/main.js
@@ -4,23 +4,22 @@ const messageInput = document.querySelector(".text_input");
 const form = document.querySelector(".client_input");
 const socket = new WebSocket("wss://echo-ws-service.herokuapp.com");
 
-
-socket.onopen = function () {
+socket.addEventListener("open", () => {
   console.log("Connected to the echo server.");
-};
+});
 
-socket.onmessage = function (event) {
+socket.addEventListener("message", event => {
   const message = document.createElement("div");
   message.innerText = event.data;
   message.classList.add("ws_message");
   messageBox.appendChild(message);
-};
+});
 
-socket.onclose = function () {
+socket.addEventListener("close", () => {
   console.log("Disconnected from the echo server.");
-};
+});
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", e => {
   e.preventDefault();
   const sentMessage = document.createElement("div");
   sentMessage.innerText = messageInput.value;
@@ -30,38 +29,36 @@ form.addEventListener("submit", function (e) {
   messageInput.value = "";
 });
 
-function getLocation() {
+const getLocation = () => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      var location = latitude + "," + longitude;
-      var apiKey = "03e081d9f3a64dd29e322f85098ab85b";
-      var url = `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${apiKey}`;
+    navigator.geolocation.getCurrentPosition(position => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const location = `${latitude},${longitude}`;
+      const apiKey = "03e081d9f3a64dd29e322f85098ab85b";
+      const url = `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${apiKey}`;
       fetch(url)
         .then(response => response.json())
         .then(data => {
           console.log(data);
-          let flagName = data.results[0].components.country_code;
-          let country = data.results[0].components.country;
-          let city = data.results[0].components.town || data.results[0].components.city || data.results[0].components.state_district;
-          let message = document.createElement("div");
-          let flag = document.createElement("img");
-          flag.setAttribute("src", `https://flagcdn.com/16x12/${flagName}.png`)
-          message.innerHTML = "I am in " + city + ", " + country + " ";
+          const flagName = data.results[0].components.country_code;
+          const country = data.results[0].components.country;
+          const city = data.results[0].components.town || data.results[0].components.city || data.results[0].components.state_district;
+          const message = document.createElement("div");
+          const flag = document.createElement("img");
+          flag.setAttribute("src", `https://flagcdn.com/16x12/${flagName}.png`);
+          message.innerHTML = `I am in ${city}, ${country} `;
           message.classList.add("client_message");
           message.appendChild(flag);
           messageBox.appendChild(message);
         });
     });
   } else {
-    let div = document.createElement("div");
-    div.innerHTML = "Geolocation is not supported by this browser.";
-    document.body.appendChild(div);
+    alert("Geolocation is not supported by this browser.");
   }
-}
+};
 
-btnLocation.addEventListener("click", (e) => {
-  getLocation()
-  e.preventDefault()
-})
+btnLocation.addEventListener("click", e => {
+  getLocation();
+  e.preventDefault();
+});
